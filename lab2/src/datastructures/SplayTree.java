6032728100dd079d36475257f2fd9c4084af146e
Joakim Berntsson <jcberntsson@gmail.com>
2015-02-19 13:00:37 +0100

    Added method

--- lab2/src/datastructures/SplayTree.java
+++ lab2/src/datastructures/SplayTree.java
@@ -1,5 +1,7 @@
 package datastructures;
 
+import testSortCol.CollectionWithGet;
+
 /**
  * Created by: Tim Kerschbaumer & Joakim Berntsson
  * Group: 6
@@ -7,5 +9,12 @@ package datastructures;
  * Date: 15-02-19
  * Time: 12:52
  */
-public class SplayTree<E extends Comparable<? super E>> extends BinarySearchTree<E> {
+public class SplayTree<E extends Comparable<? super E>> extends BinarySearchTree<E> implements CollectionWithGet<E> {
+
+    @Override
+    public E get(E e) {
+        return null;
+    }
+
+
 }
