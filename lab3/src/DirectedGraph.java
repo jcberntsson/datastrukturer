4d629b93fca90ec51c1b2c84a74ba52168a6b5d7
Joakim Berntsson <jcberntsson@gmail.com>
2015-03-03 19:53:20 +0100

    Added kruskal

--- lab3/src/DirectedGraph.java
+++ lab3/src/DirectedGraph.java
@@ -2,23 +2,60 @@
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
+    private List<E>[] edges;
 
-
-	public DirectedGraph(int noOfNodes) {
-		;
+	public DirectedGraph(int nbrOfNodes) {
+		edges = (List<E>[]) new List[nbrOfNodes];
+        for(int i = 0; i < edges.length; i++){
+            edges[i] = new ArrayList<E>();
+        }
 	}
 
 	public void addEdge(E e) {
-		;
+		edges[e.from].add(e);
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {
+        // Djikstra
 		return null;
 	}
 		
 	public Iterator<E> minimumSpanningTree() {
-		return null;
+        // Kruskals
+        int nbrOfCC = edges.length;
+        DirectedGraph<E> mst = new DirectedGraph<E>(nbrOfCC);
+        PriorityQueue<E> queue = new PriorityQueue<E>(new CompKruskalEdge<Edge>());
+        for (List<E> edge : edges) {
+            queue.addAll(edge);
+        }
+        while(nbrOfCC > 1 && !queue.isEmpty()){
+            // Find cheapest
+            E cheapest = queue.poll();
+            System.out.println(cheapest);
+            // Check if no cycle created --> Add to MST
+            if(mst.edges[cheapest.from] != mst.edges[cheapest.to]){
+                // Merge the lists
+                List<E> shortest = mst.edges[cheapest.from];
+                List<E> longest = mst.edges[cheapest.to];
+                if(shortest.size() > longest.size()){
+                    List<E> tmp = shortest;
+                    shortest = longest;
+                    longest = tmp;
+                }
+                for(E e : shortest){
+                    longest.add(e);
+                }
+                // Add the cheapest
+                longest.add(cheapest);
+                // Link all nodes to the longest list
+                for(E e : longest){
+                    mst.edges[e.from] = longest;
+                    mst.edges[e.to] = longest;
+                }
+                // Decrease number of components
+                nbrOfCC--;
+            }
+        }
+		return mst.edges[0].iterator();
 	}
-
 }
-  
--- lab3/src/ShortRoute.java
+++ lab3/src/ShortRoute.java
@@ -287,7 +287,7 @@ public class ShortRoute extends JFrame implements ActionListener {
 			// Read stops and put them in the node-table 
 			// in order to give the user a list of possible stops
 			// assume input file is correct
-			indata = new Scanner(new File("stops.noBOM.txt"), "UTF-8");
+			indata = new Scanner(new File("C:/Users/Joakim/Documents/Github/datastrukturer/lab3/out/production/lab3/stops.noBOM.txt"), "UTF-8");
 			while (indata.hasNext()) {
 				String hpl = indata.next().trim();
 				int xco = indata.nextInt();
@@ -305,7 +305,7 @@ public class ShortRoute extends JFrame implements ActionListener {
 			indata.close();
 			
 			//  Read in the lines and add to the graph
-			indata =  new Scanner(new File("lines.noBOM.txt"), "UTF-8");
+			indata =  new Scanner(new File("C:/Users/Joakim/Documents/Github/datastrukturer/lab3/out/production/lab3/lines.noBOM.txt"), "UTF-8");
 			grafen = new DirectedGraph<BusEdge>(noderna.noOfNodes());
 			while ( indata.hasNext() ) {
 				String lineNo = indata.next();
